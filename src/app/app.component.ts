037b18b7ba836d860fca9c0b61e7051e87315d6b
lukasz <lukaszsiembida@wp.pl>
2020-10-03 13:33:02 +0200

    zadanie 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,23 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  topMovies = [
-    {title: 'The Shawshank Redemption', director: 'Frank Darabont'},
-    {title: 'The Godfaher', director: 'Francis Ford Coppola'},
-    {title: 'The Godfather 2', director: 'Francis Ford Coppola'},
-    {title: 'The Dark Knight', director: 'Christopher Nolan'},
-  ];
+  tab = 0;
+
+  setTab(num: number): void {
+    this.tab = num;
+  }
+
+  isSelected(num: number): boolean {
+    return this.tab === num;
+  }
+
+  // zadanie 4
+  // topMovies = [
+  //   {title: 'The Shawshank Redemption', director: 'Frank Darabont'},
+  //   {title: 'The Godfaher', director: 'Francis Ford Coppola'},
+  //   {title: 'The Godfather 2', director: 'Francis Ford Coppola'},
+  //   {title: 'The Dark Knight', director: 'Christopher Nolan'},
+  // ];
   // zadanie 3
   // exists = true;
   // toggleExists(): void {this.exists = !this.exists;}
