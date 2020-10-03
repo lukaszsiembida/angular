3bf49e770e34bc325a348f3949d7530d34c510eb
lukasz <lukaszsiembida@wp.pl>
2020-10-03 11:51:23 +0200

    zadanie 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,18 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  exists = true;
-  toggleExists(): void {
-    this.exists = !this.exists;
-  }
+  topMovies = [
+    {title: 'The Shawshank Redemption', director: 'Frank Darabont'},
+    {title: 'The Godfaher', director: 'Francis Ford Coppola'},
+    {title: 'The Godfather 2', director: 'Francis Ford Coppola'},
+    {title: 'The Dark Knight', director: 'Christopher Nolan'},
+  ];
+  // zadanie 3
+  // exists = true;
+  // toggleExists(): void {this.exists = !this.exists;}
 }
+
+
+
+
+
--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-my-component',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyComponentComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  director: any;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
